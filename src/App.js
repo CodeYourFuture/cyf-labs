f1813dddb4a26839195eba290f42b313c42713a9
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-10-01 14:03:16 +0100

    semantic so far

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import Trainee from './components/Trainee';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <img src="./logo.svg" alt='' />
-      <div className='trainees'>
+      <section className='trainees'>
         <Trainee text="Female" percent={40} />
         <Trainee text="Minority Ethnic background" percent={78} />
         <Trainee text="Refugee / Asylum Seeker background" percent={53} />
         <Trainee text="Live below the poverty line" percent={83} />
-      </div>
-
-    </div>
+      </section>
+    </main>
   );
 }
 
--- src/components/Trainee.jsx
+++ src/components/Trainee.jsx
@@ -2,21 +2,19 @@ import "./Trainee.css";
 
 const Trainee = ({ text, percent }) => {
   return (
-    <div className="box">
-      <div className="percent">
+    <article className="box">
+      <figure className="percent">
         <svg>
           <circle cx={70} cy={70} r={70}></circle>
           <circle cx={70} cy={70} r={70} className={text}></circle>
         </svg>
-        <div className="number">
-          <h2>
-            {percent}
-            <span>%</span>
-          </h2>
-        </div>
-      </div>
-      <div className="text">{text}</div>
-    </div>
+        <h2 className="number">
+          {percent}
+          <span>%</span>
+        </h2>
+      </figure>
+      <p className="text">{text}</p>
+    </article>
   );
 };
 
